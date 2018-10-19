736c9a9d02a4d3e2f56939d46c30a46d448a55a1
Loic Gouarin <loic.gouarin@gmail.com>
2018-10-19 15:42:05 +0200

    fix setup.py

--- recipe_examples/python/base/setup.py
+++ recipe_examples/python/base/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hello',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'hello_python = script.main:main',
